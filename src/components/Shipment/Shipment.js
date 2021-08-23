5efdf2e3640c6d6789b3cffe9aec71e4517a75d6
Raisul Islam Nibir <raisulislam.cse3.bu@gmail.com>
2021-08-23 18:17:48 +0600

    Shipment page updated

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -11,7 +11,7 @@ const Shipment = () => {
   const [loggedInUser, setLoggedInUser] = useContext(userContext);
   const { id } = useParams();
   const [service, setService] = useState({});
-  const { serviceName, price, serviceDetails, image, serviceProviderEmail } = service;
+  const { serviceName, price, serviceDetails, serviceImage, serviceProviderEmail } = service;
   const [shippingData, setShippingData] = useState(null);
   const [status, setStaus] = useState('pending');
 
@@ -25,7 +25,7 @@ const Shipment = () => {
       serviceName,
       price,
       serviceDetails,
-      image,
+      serviceImage,
       serviceProviderEmail,
       paymentId,
       shipment: shippingData,
@@ -57,41 +57,6 @@ const Shipment = () => {
   return (
     <>
       <NavBar />
-      {/* <div className="container py-5">
-        <div className="row">
-          <div className="col-md-6">
-            < form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
-              < input name="name" defaultValue={loggedInUser.name} {...register('name', { required: true })} placeholder="Your Name" />
-              {errors.name && <span className="error">Name is required</span>}
-
-              < input name="email" defaultValue={loggedInUser.email} {...register('email', { required: true })} placeholder="Your Email" />
-              {errors.email && <span className="error">Email is required</span>}
-
-              < input name="address" {...register('address', { required: true })} placeholder="Your Address" />
-              {errors.address && <span className="error">Address is required</span>}
-
-              
-
-              <input className="btn btn-success" type="submit" />
-            </form >
-          </div>
-          <div className="col-md-6 mt-3">
-            <h2>Payment Method</h2>
-            <br />
-            <input type="radio" id="stripe" name="payment" value="stripe" checked />
-            <label for="stripe">Stripe</label>
-            <br />
-            <input type="radio" id="bkash" name="payment" value="bkash" />
-            <label for="bkash">bKash</label>
-            <br />
-            <input type="radio" id="creditCard" name="payment" value="creditCard" />
-            <label for="creditCard">Credit Card</label>
-            <br />
-            <br />
-            <ProcessPayment shippingData={shippingData} handlePayment={handlePaymentSuccess}></ProcessPayment>
-          </div>
-        </div>
-      </div> */}
       <div class="container col-md-7 col-lg-8 py-5">
         <div className="row">
           <div style={{ display: shippingData ? 'none' : 'block' }}>
